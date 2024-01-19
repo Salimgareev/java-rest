b2e18b0297fcc1ba5ccb35dfcae92559823f7a6f
Kamil <kamilsalimgareev270@gmail.com>
2024-01-19 16:01:11 +0500

    Базовое создание файлов проекта

---
+++ src/main/java/ru/ibs/App.java
@@ -0,0 +1,13 @@
+package ru.ibs;
+
+/**
+ * Здравствуйте, Мистер Андерсон!
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Буду предельно откровенен с вами, Мистер Андерсон. " +
+                "Вы здесь, потому что нам нужна ваша помощь");
+    }
+}
---
+++ src/test/java/ru/ibs/AppTest.java
@@ -0,0 +1,15 @@
+package ru.ibs;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Тестирование REST API
+ */
+public class AppTest {
+    @Test
+    public void restTest( )
+    {
+        System.out.println("Привет");;
+    }
+}
